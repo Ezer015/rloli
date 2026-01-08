ccf00ba07cdbc055c7efacaf1308d83e41759d59
Ezer015 <1379242357@qq.com>
2026-01-09 04:13:08 +0800

    fix: update static file path to use dynamic join for better compatibility

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { Elysia, t } from "elysia"
 import { openapi } from '@elysiajs/openapi'
+import { join } from 'path'
 
 const loliconApi = "api.lolicon.app"
 const imgEndPoint = "/setu/v2"
@@ -11,7 +12,7 @@ const proxyApi = "i.yuki.sh"
 
 export default new Elysia()
     .use(openapi())
-    .get("/static", () => Bun.file("./static/index.html"))
+    .get("/static", () => Bun.file(join(process.cwd(), 'static', 'index.html')))
     .get("/", async ({ query, status, redirect }) => {
         const url = new URL(imgEndPoint, `https://${loliconApi}`)
         url.search = new URLSearchParams(
